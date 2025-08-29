40e6aee48e3fd4378f67c3b3e39e1fa973a11c06
Juan Carlos <jcpm06@gmail.com>
2025-08-29 14:27:05 +0200

    Update OcupacionServiceImpl.java

--- src/main/java/com/melilla/gestPlanes/service/impl/OcupacionServiceImpl.java
+++ src/main/java/com/melilla/gestPlanes/service/impl/OcupacionServiceImpl.java
@@ -26,38 +26,36 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class OcupacionServiceImpl implements OcupacionService {
-	
+
 	@Autowired
 	private OcupacionRepository ocupacionRepository;
-	
+
 	@Autowired
 	private CategoriaRepository categoriaRepository;
-	
+
 	@Autowired
 	PlanService planService;
-	
-	
+
 	@Override
 	public List<Ocupacion> obtenerOcupacionesPorCategoria(Long idCategoria) {
-		
+
 		return ocupacionRepository.findAllByCategoriaIdCategoriaOrderByOcupacionAsc(idCategoria);
 	}
 
-
 	@Override
 	public List<OcupacionResponseDTO> obtenerOcupacionesPlan(Plan idPlan) {
-		
+
 		List<OcupacionResponseDTO> result = new ArrayList<OcupacionResponseDTO>();
-		
+
 		List<Ocupacion> ocupaciones = ocupacionRepository.findAllByIdPlan(idPlan);
-		
+
 		Iterator<Ocupacion> it = ocupaciones.iterator();
-		
+
 		while (it.hasNext()) {
 			Ocupacion ocupacion = it.next();
-			
+
 			OcupacionResponseDTO dto = new OcupacionResponseDTO();
-			
+
 			dto.setCategoria(ocupacion.getCategoria().getCategoria());
 			dto.setCatLab(ocupacion.getCategoria().getGrupoProfesionalPersonalLaboral());
 			dto.setIdOcupacion(ocupacion.getIdOcupacion());
@@ -66,56 +64,52 @@ public class OcupacionServiceImpl implements OcupacionService {
 			dto.setIdCategoria(ocupacion.getCategoria().getIdCategoria());
 			result.add(dto);
 		}
-		
+
 		return result;
 	}
-
 
 	@Override
 	public void borrarOcupacion(Long idOcupacion) {
 
-		Ocupacion ocupacion = ocupacionRepository.findById(idOcupacion).orElseThrow(()-> new OcupacionNotFoundException(idOcupacion));
-		
+		Ocupacion ocupacion = ocupacionRepository.findById(idOcupacion)
+				.orElseThrow(() -> new OcupacionNotFoundException(idOcupacion));
+
 		if (!ocupacion.getContratos().isEmpty()) {
 			throw new OcupacionConContratosException(idOcupacion);
-		}else
-		{
+		} else {
 			ocupacionRepository.delete(ocupacion);
 		}
-		
-	}
 
+	}
 
 	@Override
 	public Ocupacion crearOcupacion(CrearOcupacionDTO ocupacion) {
-		
-		Categoria cat = categoriaRepository.findById(ocupacion.getIdCategoria()).orElseThrow(()-> new CategoriaNotFoundException(ocupacion.getIdCategoria()));
-		
+
+		Categoria cat = categoriaRepository.findById(ocupacion.getIdCategoria())
+				.orElseThrow(() -> new CategoriaNotFoundException(ocupacion.getIdCategoria()));
+
 		Ocupacion nuevaOcupacion = new Ocupacion();
-		
+
 		nuevaOcupacion.setCategoria(cat);
-		
+
 		nuevaOcupacion.setOcupacion(ocupacion.getOcupacion());
-		
+
 		nuevaOcupacion.setOcupacionSEPE(ocupacion.getCno());
-		
+
 		nuevaOcupacion.setIdPlan(planService.getPlan(ocupacion.getIdPlan()).get());
-		
-		
+
 		return ocupacionRepository.save(nuevaOcupacion);
 	}
 
-
 	@Override
 	public Ocupacion editarOcupacion(EditarOcupacionDTO ocupacion) {
-		
-		Ocupacion ocup = ocupacionRepository.findById(ocupacion.getIdOcupacion()).orElseThrow(()-> new OcupacionNotFoundException(ocupacion.getIdOcupacion()));
-		
+
+		Ocupacion ocup = ocupacionRepository.findById(ocupacion.getIdOcupacion())
+				.orElseThrow(() -> new OcupacionNotFoundException(ocupacion.getIdOcupacion()));
+
 		ocup.setOcupacion(ocupacion.getOcupacion());
 		ocup.setOcupacionSEPE(ocupacion.getCno());
-		
-		
-		
+
 		return ocupacionRepository.save(ocup);
 	}
 
