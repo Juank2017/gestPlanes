613ac4fc7dd3f17b72265cd72fe3ea66c9c62535
Juan Carlos <jcpm06@gmail.com>
2026-03-13 14:17:10 +0100

    Crear clase PlantillaCargaCandidatos (incompleta)

---
+++ src/main/java/com/melilla/gestPlanes/model/PlantillaCargaCandidatos.java
@@ -0,0 +1,15 @@
+package com.melilla.gestPlanes.model;
+
+import lombok.Data;
+
+@Data
+public class PlantillaCargaCandidatos {
+
+	
+	
+	private String fileName;
+	
+	private boolean procesada;
+	
+	
+}
