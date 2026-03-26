a5acdfdfe9392ecb97046762e4caadc6daa56f94
Juan Carlos <jcpm06@gmail.com>
2026-03-26 09:36:52 +0100

    Update CiudadanoController.java

--- src/main/java/com/melilla/gestPlanes/controller/CiudadanoController.java
+++ src/main/java/com/melilla/gestPlanes/controller/CiudadanoController.java
@@ -130,6 +130,7 @@ public class CiudadanoController {
 				item.setDeleted(ciudadano.isDeleted());
 				item.setNotas((ciudadano.getNotas().size() >0)?true:false);
 				item.setEquipo((ciudadano.getEquipo() != null)?ciudadano.getEquipo().getNombreEquipo():"");
+				item.setEmail(ciudadano.getEmail());
 				lista.getTrabajadores().add(item);
 				
 
